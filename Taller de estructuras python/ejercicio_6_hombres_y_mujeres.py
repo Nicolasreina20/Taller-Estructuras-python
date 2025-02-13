cc52c1e2311dea1cefead7049dbbfbc328cebd82
Nicolasreina20 <nicolastrona11@gmail.com>
2025-02-12 21:27:59 -0500

    Add files via upload

---
+++ Taller de estructuras python/ejercicio_6_hombres_y_mujeres.py
@@ -0,0 +1,9 @@
+#Entradas
+mujeres=int(input("Ingrese cantidad de mujeres:"))
+hombres=int(input("Ingrese cantidad de hombres:"))
+#Caja negra
+total_estudiantes=mujeres+hombres
+porcentaje_hombres=hombres/(total_estudiantes*100)
+porcentaje_mujeres=mujere/(total_estudiantes*100)
+#Salida
+print ("El porcentaje mujeres", porcentaje_mujeres, "%", "porcentaje_hombres:",porcentaje_hombres,"%")
